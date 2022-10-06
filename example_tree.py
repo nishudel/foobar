edad86a4da2ea75dd9d64001af79b959054e22e1
Nishanth Venkatesh S <58917132+nishudel@users.noreply.github.com>
2022-10-06 14:32:27 -0400

    Add files via upload

--- example_tree.py
+++ example_tree.py
@@ -1,59 +1,64 @@
 def solutions(h,q):
+    q=list(q)
     p=[]
     #Exception
-    for elem in q:
-        if elem>=((2^h)-1) or elem<=0:
-            p.append(-1)
+    #for elem in q:
+    #    print(elem)
+    #    if elem>=((2^h)-1) or elem<=0:
+    #        p.append(-1)
         
     # Track a map left(-1) or right(0)
     #maps=[]
-    #for elem in q:
-    map=[]
-    status=False    
-    elem=q[0]
-    for level in range(h,0,-1):
+    for elem in q:
+        map=[]
+        #print(map)
+        status=False    
+        for level in range(h,0,-1):
+            if status==False:
+                track_val=(2**h)-1
+                if len(map)!=0:
+                    #print(map)
+                    #print("level",level)
+                    temp_level=h
+                    for dir in map:
+                        temp_level+=-1                    
+                        if dir=='r':
+                            #print('trackval_bef',track_val)
+                            track_val+=-1
+                            #print('trackval_aft',track_val)
+                        if dir=='l':
+                            #print('trackval_bef',track_val,-(2**(temp_level))) 
+                            track_val+=-(2**(temp_level))     
+                            #print('trackval_aft',track_val)  
+                
+                if elem<=track_val-1 and elem>track_val-(2**(level-1)):
+                    map.append('r')
+                    #print("action: right")
+                else:
+                    map.append('l')
+
+                if elem == track_val:
+                    #print(map[-2])
+                    if map[-2]=='r':
+                        p.append(track_val+1)
+                        #print("found match in right",track_val+1)
+                        #print(track_val)
+                    elif map[-1]=='l':
+                        p.append(track_val+(2**(level)))
+                        #print("found match in left",(2**(level)))
+                        #print(track_val)
+                    status=True
+                    break            
+
         if status==False:
-            track_val=(2**h)-1
-       
-            
-            print("track_val,level",track_val,level)            
-
-            if elem<=track_val-1 and elem>track_val-(2**(level-1)):
-                map.append('r')
-                #print("action: right")
-            else:
-                map.append('l')
-
-            if len(map)!=0:
-                print(map)
-                for dir in map:
-                    if dir=='r':
-                        track_val+=-1
-                    if dir=='l':
-                        track_val+=-(2**(level-1))
-
-            if elem == track_val:
-                if map[-1]=='r':
-                    p.append(track_val+1)
-                    print("found match in right")
-                    print(track_val)
-                elif map[-1]=='l':
-                    p.append(track_val+(2**(level-1)))
-                    print("found match in left")
-                    print(track_val)
-                status=True
-                break            
-
-                #print("action: left")
-    #print(map)
+            p.append(-1)
 
 
     #if elem==track_val or elem==track_val-(2^level)+1:
     #    status==True
     #    p.append(track_val+1)
-
-
-
+    for a,b in zip(p,q):
+        print(a,b)
     return p
 
 
@@ -61,7 +66,10 @@ def solutions(h,q):
 #a=[1,2]
 #h=5
 #print(solutions(h,a))
-solutions(5,[15])
+b={19,14,28}
+for a in b:
+    print(a)
+print(solutions(5,{19,14,28}))
 
 
 ## Check the input type
